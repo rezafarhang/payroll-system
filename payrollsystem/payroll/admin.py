80cd6bd4d0f281026b6f9da65759f4e3a44885a3
Reza Farhang <rezafarhangoa@gmail.com>
2024-04-19 01:36:58 +0330

    Add payroll admin classes

--- payrollsystem/payroll/admin.py
+++ payrollsystem/payroll/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from payroll import models as payroll_models
+
+
+@admin.register(payroll_models.RideIncome)
+class RideIncomeAdmin(admin.ModelAdmin):
+    list_display = ['courier', 'income', 'type', 'date']
+    list_filter = ['type']
+    search_fields = ['courier__username', 'type', 'date']
+    date_hierarchy = 'date'
+
+
+@admin.register(payroll_models.DailyIncome)
+class DailyIncomeAdmin(admin.ModelAdmin):
+    list_display = ['courier', 'income', 'date']
+    search_fields = ['courier__username', 'date']
+    date_hierarchy = 'date'
