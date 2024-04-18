4e74fa12e8de625e6ae942247d2735c8dccb77d4
Reza Farhang <rezafarhangoa@gmail.com>
2024-04-19 01:27:56 +0330

    Add payroll models

--- payrollsystem/payroll/models.py
+++ payrollsystem/payroll/models.py
@@ -1,3 +1,23 @@
-from django.db import models
+from datetime import date
 
-# Create your models here.
+from django.db import models
+from django.core.validators import MinValueValidator
+from django.contrib.auth.models import User
+
+
+class RideIncome(models.Model):
+    INCOME_TYPE_CHOICES = (
+        (1, 'income'),
+        (2, 'deficit'),
+        (3, 'increment'),
+    )
+    courier = models.ForeignKey(User, related_name='ride_incomes', on_delete=models.CASCADE)
+    income = models.IntegerField(validators=[MinValueValidator(1)])
+    type = models.IntegerField(choices=INCOME_TYPE_CHOICES)
+    date = models.DateField(default=date.today())
+
+
+class DailyIncome(models.Model):
+    courier = models.ForeignKey(User, related_name='daily_incomes', on_delete=models.CASCADE)
+    income = models.IntegerField(validators=[MinValueValidator(1)])
+    date = models.DateField(default=date.today(), unique=True, db_index=True)
