b81931d6fdf86d201426d52e7ee15a22e8df8cd4
Reza Farhang <rezafarhangoa@gmail.com>
2024-04-20 01:00:27 +0330

    Add payroll urls

--- payrollsystem/payroll/urls.py
+++ payrollsystem/payroll/urls.py
@@ -3,8 +3,9 @@ from django.urls import path
 from payroll import views
 
 urlpatterns = [
-    path('api/ride-income/', views.RideIncomeView.as_view(), name='RideIncome'),
-    path('api/daily-income/', views.DailyIncomeList.as_view(), name='DailyIncome'),
-    path('api/daily-income/<int:courier_id>/', views.DailyIncomeItem.as_view(), name='dailyincome-item'),
+    path('api/ride-income/', views.RideIncomeView.as_view(), name='ride-income'),
+    path('api/daily-income/', views.DailyIncomeList.as_view(), name='daily-income-list'),
+    path('api/daily-income/<int:courier_id>/', views.DailyIncomeItem.as_view(), name='daily-income-item'),
+    path('api/weekly-income/<int:courier_id>/', views.WeeklyIncomeList.as_view(), name='weekly-income-item'),
 
 ]
