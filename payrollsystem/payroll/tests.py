d97625230d10b9efd64db1c8398a75accb06919a
Reza Farhang <rezafarhangoa@gmail.com>
2024-04-20 00:58:56 +0330

    Add daily income tests

--- payrollsystem/payroll/tests.py
+++ payrollsystem/payroll/tests.py
@@ -1,3 +1,43 @@
-from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from rest_framework.test import APITestCase
+from rest_framework import status
+
+from payroll import models as payroll_models
+
+class DailyIncomeListTestCase(APITestCase):
+
+    def setUp(self):
+        self.courier_id = 1
+        self.courier_username = 'reza'
+        self.date = '2024-01-01'
+
+    def test_get_daily_income_item(self):
+
+
+        ride_income_url = reverse('ride-income')
+        list_of_ride_income_data = [
+            {'courier': 1, 'income': 100, 'type': 2, 'date': '2024-01-01'},
+            {'courier': 1, 'income': 500, 'type': 1, 'date': '2024-01-01'},
+            {'courier': 1, 'income': -200, 'type': 3, 'date': '2024-01-01'},
+            {'courier': 2, 'income': 150, 'type': 1, 'date': '2024-01-01'}
+        ]
+
+        for data in list_of_ride_income_data:
+            self.client.force_authenticate(user=None)
+            self.client.post(ride_income_url, data, format='json')
+
+
+        daily_income_url = reverse('daily-income-item', kwargs={'courier_id': self.courier_id})
+        daily_income_url += f'?date={self.date}'  # Add date query parameter to the URL
+        self.client.force_authenticate(user=None)
+        response = self.client.get(daily_income_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['courier']['username'], self.courier_username)
+        self.assertEqual(response.data['income'], 400)
+        self.assertEqual(response.data['date'], self.date)
+        self.assertEqual(
+            payroll_models.DailyIncome.objects.filter(date=self.date, courier_id=self.courier_id).count(), 1)
+
+
+
